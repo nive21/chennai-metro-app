db1edb3dd716ed8ca723fcbe3cd825e636f3f6e5
Nivedhitha <nive.mathan21@gmail.com>
2025-07-22 10:17:36 +0530

    update font and background color

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData baseDark = ThemeData.dark();
     return MaterialApp(
       title: 'Chennai Metro App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: baseDark.copyWith(
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        textTheme: baseDark.textTheme.apply(fontFamily: 'Inter'),
+        colorScheme: const ColorScheme.dark().copyWith(
+          primary: Colors.tealAccent,
+          secondary: Colors.amberAccent,
+          onPrimary: Colors.black,
+          onSecondary: Colors.black,
+          surface: const Color(0xFF121212),
+          onSurface: Colors.white,
+        ),
       ),
       home: const HomeScreen(),
     );
